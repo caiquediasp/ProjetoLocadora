388832f028439a974f5229357fe0a8c5eba41798
CaiqueDiasp <caiquediasp@gmail.com>
2024-05-10 10:52:06 -0300

    Small Changes | Trying to fix docker

--- src/main/java/com/locadora/ProjetoLocadora/repository/EstoqueRepository.java
+++ src/main/java/com/locadora/ProjetoLocadora/repository/EstoqueRepository.java
@@ -7,22 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EstoqueRepository extends JpaRepository<Pecas, Integer> {
-    @Query(value = "SELECT quantidade < :quantidade FROM tb_estoque_andaime WHERE tamanho = :tamanho"
-            , nativeQuery = true)
-    public Boolean verificaDisponibilidadeAndaime(int tamanho, int quantidade);
-
-    @Query(value = "SELECT quantidade < :quantidade FROM tb_estoque_escora WHERE tamanho = :tamanho"
-            , nativeQuery = true)
-    public Boolean verificaDisponibilidadeEscora(int tamanho, int quantidade);
-
-    @Query(value = "SELECT quantidade < :quantidade FROM tb_estoque_plataforma WHERE tamanho = :tamanho"
-            , nativeQuery = true)
-    public Boolean verificaDisponibilidadePlataforma(int tamanho, int quantidade);
-
-    @Query(value = "SELECT quantidade < :quantidade FROM tb_estoque_roldana",
-            nativeQuery = true)
-    public Boolean verificaDisponibilidadeRoldana(int quantidade);
-
     @Query(value = "SELECT quantidade FROM tb_estoque_andaime WHERE tamanho = :tamanho"
             , nativeQuery = true)
     public Integer verificaEstoqueAndaime(int tamanho);
--- src/main/java/com/locadora/ProjetoLocadora/validations/EstoqueValidation.java
+++ src/main/java/com/locadora/ProjetoLocadora/validations/EstoqueValidation.java
@@ -13,26 +13,22 @@ public class EstoqueValidation {
 
     public void verificaDisponibilidadePecas(Pecas pecas) {
         if(pecas.getAndaime().getQtdAndaime() > 0)
-            if(!estoqueRepository.verificaDisponibilidadeAndaime(
-                    pecas.getAndaime().getTamanhoAndaime(), pecas.getAndaime().getQtdAndaime()))
+            if(estoqueRepository.verificaEstoqueAndaime(
+                    pecas.getAndaime().getTamanhoAndaime()) < pecas.getAndaime().getQtdAndaime())
                 throw new QuantidadeInvalidaException("andaimes");
 
         if(pecas.getEscora().getQtdEscora() > 0 )
-            if (!estoqueRepository.verificaDisponibilidadeEscora(
-                    pecas.getEscora().getTamanhoEscora(), pecas.getEscora().getTamanhoEscora()))
+            if (estoqueRepository.verificaEstoqueEscora(
+                    pecas.getEscora().getTamanhoEscora()) < pecas.getEscora().getQtdEscora())
                 throw new QuantidadeInvalidaException("escoras");
 
         if (pecas.getPlataforma().getQtdPlataforma() > 0)
-            if(!estoqueRepository.verificaDisponibilidadePlataforma(
-                    pecas.getPlataforma().getTamanhoPlataforma(), pecas.getPlataforma().getQtdPlataforma()))
+            if(estoqueRepository.verificaEstoquePlataforma(
+                    pecas.getPlataforma().getTamanhoPlataforma()) < pecas.getPlataforma().getQtdPlataforma())
                 throw new QuantidadeInvalidaException("plataformas");
 
         if(pecas.getRoldana().getQtdRoldana() > 0)
-            if(estoqueRepository.verificaDisponibilidadeRoldana(
-                    pecas.getRoldana().getQtdRoldana()))
+            if(estoqueRepository.verificaEstoqueRoldana() < pecas.getRoldana().getQtdRoldana())
                 throw new QuantidadeInvalidaException("roldanas");
-
     }
-
-
 }
