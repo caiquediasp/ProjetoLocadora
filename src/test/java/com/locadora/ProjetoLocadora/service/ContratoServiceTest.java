2795a79565938d35e0a324f64ced897e847e71a0
caiquediasp <caiquediasp@gmail.com>
2024-07-15 20:31:35 -0300

    Keep improving test

--- src/test/java/com/locadora/ProjetoLocadora/service/ContratoServiceTest.java
+++ src/test/java/com/locadora/ProjetoLocadora/service/ContratoServiceTest.java
@@ -8,54 +8,47 @@ import com.locadora.ProjetoLocadora.util.pecas.Andaime;
 import com.locadora.ProjetoLocadora.util.pecas.Escora;
 import com.locadora.ProjetoLocadora.util.pecas.Plataforma;
 import com.locadora.ProjetoLocadora.util.pecas.Roldana;
-
 import com.locadora.ProjetoLocadora.validations.ContratoStatusValidation;
 import com.locadora.ProjetoLocadora.validations.CpfValidation;
 import com.locadora.ProjetoLocadora.validations.EstoqueValidation;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThatList;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ContratoServiceTest {
     @InjectMocks
     private ContratoService contratoService;
-
     @Mock
     private ContratoStatusValidation contratoStatusValidation;
-
     @Mock
     private CpfValidation cpfValidation;
-
     @Mock
     private EstoqueValidation estoqueValidation;
-
     @Mock
     private ContratanteRepository contratanteRepository;
-
     @Mock
     private EnderecoRepository enderecoRepository;
-
     @Mock
     private ContratoRepository contratoRepository;
 
-    Contratante contratante = new Contratante("05433755363", "nome", "(xx)xxxxx-xxxx");
+    Contratante contratante = new Contratante("42145065024", "nome", "(xx)xxxxx-xxxx");
     Endereco endereco = new Endereco("12345-678", "bairro", "rua", 1234);
     Andaime andaime = new Andaime(5, 1);
     Escora escora = new Escora(5, 1);
@@ -63,7 +56,6 @@ class ContratoServiceTest {
     Roldana roldana = new Roldana(5);
     Pecas pecas = new Pecas(andaime, escora, plataforma, roldana);
     Contrato contrato = new Contrato(LocalDate.now(), LocalDate.now().plusDays(5L), FormaPagamento.PIX, endereco, contratante, pecas);
-
 
     @BeforeEach
     void setup() {
@@ -92,58 +84,81 @@ class ContratoServiceTest {
 
     @Test
     void listarTodosContratos() {
-        assertThat(contratoService.listarTodosContratos()).isNotEmpty();
+        List<Contrato> listaTeste = new ArrayList<Contrato>();
+        listaTeste.add(contrato);
+        when(contratoRepository.findAll()).thenReturn(listaTeste);
+
+        List<Contrato> listaResultado = contratoService.listarTodosContratos();
+
+        verify(contratoRepository).findAll();
+        assertThatList(listaResultado).isNotEmpty();
     }
 
     @Test
     void listarContratosDoContratante() {
-        assertThat(contratoService.listarContratosDoContratante("05433755363")).isNotEmpty();
+        List<Contrato> listaTeste = new ArrayList<Contrato>();
+        listaTeste.add(contrato);
+        when(contratoRepository.listarContratosDoContratante(contratante.getCpf())).thenReturn(listaTeste);
+
+        List<Contrato> listaResultado = contratoService.listarContratosDoContratante(contratante.getCpf());
+
+        verify(contratoRepository).listarContratosDoContratante(contratante.getCpf());
+        assertThatList(listaResultado).isNotEmpty();
     }
 
     @Test
     void listarContratosDoEndereco() {
-        List<Contrato> listaTeste = contratoService.listarContratosDoContratante("05433755363");
-        Contrato c = listaTeste.get(0);
-        assertThat(contratoService.listarContratosDoEndereco(c.getEndereco().getId())).isNotEmpty();
+        List<Contrato> listaTeste = new ArrayList<Contrato>();
+        listaTeste.add(contrato);
+        when(contratoRepository.listarContratosDoEndereco(contrato.getEndereco().getId())).thenReturn(listaTeste);
+
+        List<Contrato> listaResultado = contratoService.listarContratosDoEndereco(contrato.getEndereco().getId());
+
+        verify(contratoRepository).listarContratosDoEndereco(contrato.getEndereco().getId());
+        assertThatList(listaResultado).isNotEmpty();
     }
 
     @Test
     void listarContratosAtivos() {
-        assertThat(contratoService.listarContratosAtivos()).isNotEmpty();
+        List<Contrato> listaTeste = new ArrayList<Contrato>();
+        listaTeste.add(contrato);
+        when(contratoRepository.listarContratosAtivos()).thenReturn(listaTeste);
+
+        List<Contrato> listaResultado = contratoService.listarContratosAtivos();
+
+        verify(contratoRepository).listarContratosAtivos();
+        assertThatList(listaResultado).isNotEmpty();
     }
 
     @Test
     void listarContratosVencidos() {
-        contrato.setStatus("VENCIDO");
-        contratoService.adicionarContrato(contrato);
+        List<Contrato> listaTeste = new ArrayList<Contrato>();
+        listaTeste.add(contrato);
+        when(contratoRepository.listarContratosVencidos()).thenReturn(listaTeste);
 
-        assertThat(contratoService.listarContratosVencidos()).isNotEmpty();
+        List<Contrato> listaResultado = contratoService.listarContratosVencidos();
+
+        verify(contratoRepository).listarContratosVencidos();
+        assertThatList(listaResultado).isNotEmpty();
     }
 
     @Test
     void buscarContratoPorId() {
-        List<Contrato> listaTeste = contratoService.listarContratosDoContratante("05433755363");
-        Contrato c = listaTeste.get(0);
+        when(contratoRepository.findById(contrato.getId())).thenReturn(Optional.ofNullable(contrato));
 
-        assertThat(contratoService.buscarContratoPorId(c.getId())).isNotNull();
+        Contrato resultado = contratoService.buscarContratoPorId(contrato.getId());
+
+        verify(contratoRepository).findById(contrato.getId());
+        assertNotNull(resultado);
     }
 
     @Test
     void renovarContrato() {
-        List<Contrato> listaTeste = contratoService.listarContratosDoContratante("05433755363");
-        Contrato c = listaTeste.get(0);
-        pecas.getAndaime().setQtdAndaime(10);
-        pecas.getEscora().setQtdEscora(15);
-
-        assertThat(contratoService.renovarContrato(c.getId(), pecas,
-                LocalDate.now().plusDays(10), LocalDate.now().plusDays(27), FormaPagamento.AVISTA)).isNotNull();
+        
     }
 
     @Test
     void excluirContrato() {
-        List<Contrato> listaTeste = contratoService.listarContratosDoContratante("05433755363");
-        Contrato c = listaTeste.get(0);
 
-        assertThat(contratoService.excluirContrato(c.getId())).isNotNull();
     }
 }
